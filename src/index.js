b3e8f6f8f3f8d34c539a1527700fd9e0ac182021
三磊 <zhanghao63271202@gmail.com>
2021-10-19 17:29:24 +0800

    测试

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// const name = 'Josh Perez';
+// const element = <h1>Hello, {name}</h1>;
+// ReactDOM.render(
+//     element,
+//     document.getElementById('root')
+// );
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+}
+// const user = false
+
+function getGreeting(user) {
+    if (user) {
+        return <h1>Hello, {formatName(user)}!</h1>
+    }
+    return <h1>Hello, Stranger!</h1>
+}
+
+const element = (
+    getGreeting(user)
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    element,
+    document.getElementById('root')
+)
