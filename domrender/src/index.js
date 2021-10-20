9a95296f983eefb95e4c75387e2031fc94a28561
三磊 <zhanghao63271202@gmail.com>
2021-10-20 14:48:57 +0800

    DOM渲染机制

--- domrender/src/index.js
+++ domrender/src/index.js
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(
+/* hello, world */
+// const element = <h1>Hello, World! </h1>
+//
+// ReactDOM.render(
+//     element,
+//     document.getElementById('root')
+// );
 
-);
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000)
